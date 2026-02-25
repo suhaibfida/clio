ac0c4b8865e5f93dd8e3ea0fe0c177980671b5d7
suhaibfida <suhaibfida33@outlook.com>
2026-02-26 01:30:02 +0530

    add page home

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-18 w-10">Hello</div>
+      <div className="flex justify-center h-96 w-80 font-bold  bg-black text-purple-900 ">
+        <span className="border border-gray-300 bg-gray-400 text-2xl mt-5 h-9 rounded-4xl pl-4 pr-4">
+          Clio
+        </span>
+      </div>
     </>
   );
 }
